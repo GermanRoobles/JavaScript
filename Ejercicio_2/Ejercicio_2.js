76565da71fc049f129ae484ef9d84aa086f9577b
GermanRoobles <117644675+GermanRoobles@users.noreply.github.com>
2022-12-15 11:59:40 +0100

    Add files via upload

---
+++ Ejercicio_2/Ejercicio_2.js
@@ -0,0 +1,14 @@
+const yo = {
+    nombre: "Germán",
+    edad: 19,
+    isDeveloper: false,
+    fecha_nacimiento: Date("2003 10 15"),
+    libro_fav: {
+        titulo: "Canción de hielo y fuego",
+        autor: "George R. R. Martin",
+        fecha: Date("1996"),
+        url: "https://www.amazon.es/Juego-Tronos-Canci%C3%B3n-hielo-fuego/dp/8496208966"
+    }
+}
+
+console.log(yo)
