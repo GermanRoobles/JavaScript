3c46ca71b8c6689514fd8216d617c05bf5bd2f16
GermanRoobles <117644675+GermanRoobles@users.noreply.github.com>
2022-12-21 10:34:40 +0100

    Add files via upload

---
+++ Ejercicio_5/ejercicio_5.js
@@ -0,0 +1,10 @@
+let altura_cm = 187;
+let altura_m = 1.87;
+let peso = 65.8;
+let altura_rd = altura_m.toFixed();
+console.log(altura_rd);
+let peso_rd = Math.floor(peso);
+console.log(peso_rd);
+
+const maxvalor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(maxvalor);
